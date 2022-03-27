4053d892a0bf89370764b2355f09f996b8652450
lonely-code-cube <poppyseed7112004@gmail.com>
2022-03-27 23:22:55 +0530

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+//!
+//!
+//! #Usage
+//!
+//! ```
+//! use swas::say;
+//!
+//! fn main() {
+//!    println! ("{}", swas::say());
+//! }
+//! ```
+
+/// The only function in this library
+/// # Example
+/// ```
+/// println! ("{}", swas::say());
+/// ```
+pub fn say() -> String {
+    String::from("69 haha very funny")
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use swas::say;
+
+fn main() {
+    println! ("{}", swas::say());
+}
